0e230efe95141c46de462c014ca68e1a878da0d8
sercan-works <sercantas1026@gmail.com>
2022-02-02 21:16:32 +0300

    3

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 import requests
 from decouple import config
+from pprint import pprint
+from .models import City
 # Create your views here.
 
 def index(request):
+    cities = City.objects.all()
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric"
-    city = "Berlin"
-    response = requests.get(url.format(city, config("API_KEY")))
-    content = response.json()
-    print(content)
-    print(type(content))
+    # city = "Eskişehir"
+    
+    # response = requests.get(url.format(city, config("API_KEY")))
+    # content = response.json()
+    # pprint(content)
+    # print(type(content))
+    
+    for city in cities:
+        print(city)
+        response = requests.get(url.format(city, config("API_KEY")))
+        content = response.json()
+        pprint(content)
+        print(type(content))
+    
     return render(request, "weatherapp/index.html")
 
